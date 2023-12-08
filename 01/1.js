92a5cb911c026944251e36cb37299d7503eddff4
Arturs Vonda <github@artursvonda.lv>
2023-12-08 21:35:05 +0200

    Add prettier

--- 01/1.js
+++ 01/1.js
@@ -1,24 +1,23 @@
-import {readFile} from "node:fs/promises";
+import { readFile } from "node:fs/promises";
 
 const main = async () => {
-    const input = (await readFile('./input.txt')).toString('utf-8')
+	const input = (await readFile("./input.txt")).toString("utf-8");
 
-    const inputParsed = input.split('\n').map((item) => {
-        let digits = item.replaceAll(/\D/g, '');
-        const firstDigit = digits[0];
-        const lastDigit = digits[digits.length - 1];
+	const inputParsed = input.split("\n").map((item) => {
+		let digits = item.replaceAll(/\D/g, "");
+		const firstDigit = digits[0];
+		const lastDigit = digits[digits.length - 1];
 
-        const number = parseInt(firstDigit + lastDigit, 10);
+		const number = parseInt(firstDigit + lastDigit, 10);
 
-        return isNaN(number) ? 0 : number;
-    });
+		return isNaN(number) ? 0 : number;
+	});
 
-    const sum = inputParsed.reduce((acc, item) => {
-        return acc + item;
-    }, 0)
+	const sum = inputParsed.reduce((acc, item) => {
+		return acc + item;
+	}, 0);
 
-    console.log(sum);
-
-}
+	console.log(sum);
+};
 
 main();
