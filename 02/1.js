2a30d3370eab7e6f2f5ddd5937ec767272b5615f
Arturs Vonda <github@artursvonda.lv>
2023-12-08 21:35:08 +0200

    Day 2

---
+++ 02/1.js
@@ -0,0 +1,40 @@
+import { readFile } from "node:fs/promises";
+
+// 12 red cubes, 13 green cubes, and 14 blue cubes
+const max = {
+	red: 12,
+	green: 13,
+	blue: 14,
+};
+
+const main = async () => {
+	const input = (await readFile("./input.txt")).toString("utf-8");
+	let possibleGames = 0;
+
+	input.split("\n").forEach((item) => {
+		if (item === "") {
+			return;
+		}
+
+		// Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+		const [gameString, cubesString] = item.split(":");
+		const game = parseInt(gameString.match(/\d+/)[0], 10);
+
+		const valid = cubesString.split(";").every((subsets) => {
+			return subsets.split(",").every((subset) => {
+				const [amountString, color] = subset.trim().split(" ");
+				const amount = parseInt(amountString, 10);
+
+				return amount <= max[color];
+			});
+		});
+
+		if (valid) {
+			possibleGames += game;
+		}
+	});
+
+	console.log(possibleGames);
+};
+
+main();
---
+++ 02/2.js
@@ -0,0 +1,42 @@
+import { readFile } from "node:fs/promises";
+
+const main = async () => {
+	const input = (await readFile("./input.txt")).toString("utf-8");
+
+	const result = input.split("\n").reduce((acc, item) => {
+		if (item === "") {
+			return acc;
+		}
+
+		// Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+		const [, cubesString] = item.split(":");
+
+		const max = cubesString.split(";").reduce(
+			(acc, subsets) => {
+				const subsetMap = Object.fromEntries(
+					subsets.split(",").map((subset) => {
+						const [amountString, color] = subset.trim().split(" ");
+						const amount = parseInt(amountString, 10);
+
+						return [color, amount];
+					}),
+				);
+
+				return {
+					red: Math.max(acc.red, subsetMap.red || 0),
+					green: Math.max(acc.green, subsetMap.green || 0),
+					blue: Math.max(acc.blue, subsetMap.blue || 0),
+				};
+			},
+			{ red: 0, green: 0, blue: 0 },
+		);
+
+		const power = max.red * max.green * max.blue;
+
+		return acc + power;
+	}, 0);
+
+	console.log(result);
+};
+
+main();
